a8840dac56be339a6c3fbf9ffe0c60775f083a61
PhamVuThuNguyet <pvtnguyet.19it1@vku.udn.vn>
2022-12-08 10:07:14 +0700

    Update index server

--- web-app/index.js
+++ web-app/index.js
@@ -6,11 +6,13 @@
 'use strict';
 
 const app = require('./app');
+const network = require('./network/network');
 
 //declare port
 const port = process.env.PORT || 8000;
 
 //run app on port
-app.listen(port, function () {
+app.listen(port, async function () {
+    await network.initLedger();
     console.log('app running on port: %d', port);
 });
